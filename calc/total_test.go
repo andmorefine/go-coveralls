5405ec3108cdf118d078c85aed4c47d22c5cdb7e
7kaji <hirotonbuu@gmail.com>
2019-12-15 15:00:51 +0900

    add tests for total_sum

--- calc/total_test.go
+++ calc/total_test.go
@@ -4,5 +4,12 @@ import "testing"
 
 // TotalSum は加算のための手続き
 func TestTotalSum(t *testing.T) {
+	var result int
 
+	result = TotalSum(1, 2)
+	if result != 3 {
+		t.Errorf("totalSum failed. expect:%d, actual:%d", 3, result)
+	}
+
+	t.Logf("result is %d", result)
 }
